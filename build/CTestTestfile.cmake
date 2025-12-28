41952532cba98004bb1ad0bf939ab8b001b73513
dizzyzzh <dizzyzzh@gmail.com>
2025-12-28 20:46:51 +0800

    add some cmake test files

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,5 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(unittest_add "C:/Code/googletest_tutorial/build/unittest_add.exe")
-set_tests_properties(unittest_add PROPERTIES  _BACKTRACE_TRIPLES "C:/Code/googletest_tutorial/CMakeLists.txt;17;add_test;C:/Code/googletest_tutorial/CMakeLists.txt;0;")
+set_tests_properties(unittest_add PROPERTIES  _BACKTRACE_TRIPLES "C:/Code/googletest_tutorial/CMakeLists.txt;18;add_test;C:/Code/googletest_tutorial/CMakeLists.txt;0;")
 subdirs("gtest/googletest")
