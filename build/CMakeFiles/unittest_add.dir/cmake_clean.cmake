01f2cb4fff979df78b56b13454d4670d4116060f
dizzyzzh <dizzyzzh@gmail.com>
2025-12-28 20:41:19 +0800

    add some test files

---
+++ build/CMakeFiles/unittest_add.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unittest_add.dir/src/fuction/add.cpp.obj"
+  "CMakeFiles/unittest_add.dir/src/fuction/add.cpp.obj.d"
+  "CMakeFiles/unittest_add.dir/src/test/unittest_add.cpp.obj"
+  "CMakeFiles/unittest_add.dir/src/test/unittest_add.cpp.obj.d"
+  "libunittest_add.dll.a"
+  "unittest_add.exe"
+  "unittest_add.exe.manifest"
+  "unittest_add.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unittest_add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Code/googletest_tutorial
+# Build directory: C:/Code/googletest_tutorial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittest_add "C:/Code/googletest_tutorial/build/unittest_add.exe")
+set_tests_properties(unittest_add PROPERTIES  _BACKTRACE_TRIPLES "C:/Code/googletest_tutorial/CMakeLists.txt;17;add_test;C:/Code/googletest_tutorial/CMakeLists.txt;0;")
+subdirs("gtest/googletest")
